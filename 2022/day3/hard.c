20735e0a752acef61aa03f6432fdf713446ca59b
user <user@github.com>
2022-12-03 19:13:09 +0100

    fixed hard

--- 2022/day3/hard.c
+++ 2022/day3/hard.c
@@ -1,101 +1,36 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdbool.h>
 #include <string.h>
-#include <stdlib.h>
-
-
-typedef struct {
-    char content[256];
-    unsigned int len;
-    unsigned long bits;
-} rucksack;
-
-
-typedef struct {
-    rucksack** rs;
-    unsigned int len;
-} group;
-
-
-group* group_init(int size)
-{
-    group* grp = malloc(sizeof(group));
-    grp->len = size;
-    grp->rs = malloc(grp->len * 8);
-
-    // allocate zeroed rucksack's
-    for (int i=0; i < grp->len; i++)
-        grp->rs[i] = calloc(sizeof(rucksack), 1);
-
-    return grp;
-}
-
-
-group* group_read(group* grp, FILE* file)
-{
-    for (int i=0; i < grp->len; i++)
-    {
-        rucksack* rs = grp->rs[i];
-        if (!fgets(rs->content, sizeof(rs->content), file))
-            return NULL;
-        
-        // remove \n byte
-        rs->len = strlen(rs->content)-1;
-        rs->content[rs->len] = '\0';
-    }
-
-    return grp; 
-}
-
-
-group* group_init_bits(group* grp)
-{
-    for (int i=0; i < grp->len; i++)
-    {
-        rucksack* rs = grp->rs[i];
-        rs->bits = 0;
-
-        for (int i=0; i < rs->len; i++)
-        {
-            // each character get a bit
-            rs->bits |= (unsigned long)pow(2, rs->content[i]-65);
-        }
-    }
-
-    return grp;
-}
-
-
-char group_find_dup(group* grp)
-{
-    unsigned long comp = 0xffffffffffffffff;  // 2**64-1
-    
-    // strike out every character that's not in the bitfields
-    for (int i=0; i < grp->len; i++)
-        comp &= grp->rs[i]->bits;
-
-    return 65 + log2(comp) / log2(2); // 65 + logbase(2, comp)
-}
-
 
 int main()
 {
     FILE* file = fopen("input.txt", "r");
-    group* grp = group_init(3);
+    char buf[256];
     int sum = 0;
+    unsigned long bits = 0xffffffffffffffff; // 2**64-1
 
-    while (group_read(grp, file))
+    for (int i=1; fgets(buf, sizeof(buf), file); i++)
     {
-        group_init_bits(grp);
-   
-        char dup = group_find_dup(grp);
-        int incr = dup&31;  // get priority
-        if (!(dup & 32)) // is upper
-            incr += 26;
+        unsigned long rucksack = 0;
+        for (int j=0; j < strlen(buf)-1; j++)
+        {
+            // each character get a bit
+            rucksack |= (unsigned long)pow(2, buf[j]-65);
+        }
 
-        sum += incr;
-        printf("%c (%d)\n", dup, incr);
+        bits &= rucksack;
+        if (i % 3 == 0)
+        {
+            char dup = 65 + log2(bits) / log2(2); // 65 + logbase(2, comp)
+            int incr = dup&31;  // get priority
+            if (!(dup & 32)) // is upper
+                incr += 26;
+
+            sum += incr;
+            printf("%c (%d)\n", dup, incr);
+        
+            bits = 0xffffffffffffffff;
+        }
     }
 
     printf("sum: %d\n", sum);
